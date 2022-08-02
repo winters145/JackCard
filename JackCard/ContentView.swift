32a6f8b98d69296c81418c351bf04ea42cf827b9
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-02 16:30:07 +0100

    Business card app

--- JackCard/ContentView.swift
+++ JackCard/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Color("BackgroundColour")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("JackPic")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 175, height: 175)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Jack Winterschladen")
+                    .font(Font.custom("PlayfairDisplay-Italic", size: 35))
+                    .bold()
+                    .padding()
+                .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+44 77 2223 6999", imageName: "phone.fill")
+                InfoView(text: "winterschladen.j@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portraitUpsideDown)
+            ContentView()
+                .previewInterfaceOrientation(.portraitUpsideDown)
+        }
     }
 }
---
+++ JackCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  JackCard
+//
+//  Created by Jack Winterschladen on 02/08/2022.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(Color("BackgroundColour"))
+                Text(text)
+            })
+            .padding(.horizontal, 15.0)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
