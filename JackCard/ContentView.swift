03d260b4543dc3bee374c6ed975f267fa22a54c9
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-02 15:19:06 +0100

    Initial Commit

---
+++ JackCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JackCard
+//
+//  Created by Jack Winterschladen on 02/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JackCard/JackCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JackCardApp.swift
+//  JackCard
+//
+//  Created by Jack Winterschladen on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct JackCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
